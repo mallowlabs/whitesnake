223b4bf1e3f764829c4fc458a2ca14889507c86e
Kenta Murata <mrkn@cookpad.com>
2013-06-10 09:17:42 +0900

    rails new whitesnake -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whitesnake::Application.config.secret_key_base = '0574f71c13b0362bfc1e933dec7d1433893a061f033f5ba4f3890714dfcf5e17a2179ad1bf127b2f53bb424b196624220161f30ba00ccba65d2aabf60592365c'
