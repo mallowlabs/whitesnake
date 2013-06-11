fa980e053e9474412318a886ac1ae89c877ec90d
Kenta Murata <mrkn@cookpad.com>
2013-06-11 22:05:49 +0900

    Add skelton examples of User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.from_omniauth' do
+  end
+
+  describe '.create_from_omniauth' do
+    it 'creates a new user from the OmniAuth information'
+  end
+
+  describe '#each_repository' do
+    context 'without a block' do
+      it 'returns an Enumerable'
+    end
+
+    context 'with a block' do
+      it 'iterates each repository of the user'
+    end
+  end
 end
