db80cd95369655175617cc0c2d8c53120035d526
Kenta Murata <mrkn@cookpad.com>
2013-07-19 21:07:22 +0900

    Enable rack-livereload on development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Whitesnake::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # rack-livereload
+  config.middleware.insert_after(ActionDispatch::Static, Rack::LiveReload)
 end
