8fffaec364994fc363f9cc50ed88ddf10a1270b2
Kenta Murata <mrkn@cookpad.com>
2013-07-22 21:36:48 +0900

    Require login for actions in UsersController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,11 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def require_login
+    return if current_user
+    redirect_to root_path
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
