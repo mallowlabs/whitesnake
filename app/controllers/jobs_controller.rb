521d684ccc7ea8041581fdf15338fcf0cd873515
Kenta Murata <mrkn@cookpad.com>
2013-07-22 21:56:34 +0900

    Modify to use whitesnake.yml in repositories

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,9 +1,15 @@
+require 'base64'
+require 'octokit_helper'
+
 class JobsController < ApplicationController
+  include OctokitHelper::Controller
+
+  before_action :fetch_job_setting, only: [ :create, :destroy ]
+
   def create
-    repository = current_user.repository(params[:repository_id])
     @response = Net::HTTP.start('localhost', 8080) do |http|
-      http.post("/createItem?name=#{CGI.escape params[:repository_id]}",
-                config_xml(repository),
+      http.post("/createItem?name=#{CGI.escape @job_setting['name']}",
+                config_xml(@job_setting, @repository).tap {|x| puts x},
                 'Content-Type' => 'text/xml')
     end
 
@@ -17,7 +23,7 @@ class JobsController < ApplicationController
 
   def destroy
     response = Net::HTTP.start('localhost', 8080) do |http|
-      http.post("/job/#{CGI.escape params[:repository_id]}/doDelete", '')
+      http.post("/job/#{CGI.escape @job_setting['name']}/doDelete", '')
     end
     case response
     when Net::HTTPSuccess, Net::HTTPRedirection
@@ -29,7 +35,35 @@ class JobsController < ApplicationController
 
   private
 
-  def config_xml(repository)
-    render_to_string 'jobs/config', layout: false, locals: { repository: repository }
+  def fetch_job_setting
+    @repository = octokit_client.repository(params[:repository_id])
+    response = octokit_client.content(params[:repository_id], path: 'whitesnake.yml')
+    if response[:type] == 'file'
+      case response[:encoding]
+      when 'base64'
+        content = Base64.decode64(response[:content])
+      else
+        content = response[:content]
+      end
+      @job_setting = Psych.load(content)
+    else
+      @job_setting = default_job_setting(@repository)
+    end
+  rescue => error
+    p error
+    render json: { message: error.message }, status: 404
+  end
+
+  def default_job_setting(repository)
+    {
+      'name'     => repository[:name],
+      'branches' => %w[origin/master],
+      'rbenv'    => %w[system],
+      'script'   => %w[bundle && bundle exec rake spec],
+    }
+  end
+
+  def config_xml(job_setting, repository)
+    render_to_string 'jobs/config', layout: false, locals: { job_setting: job_setting, repository: repository }
   end
 end
