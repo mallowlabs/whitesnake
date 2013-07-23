c99dfa43ea487bb6c59f117ca1a101ec744b683c
Kenta Murata <mrkn@mrkn.jp>
2013-07-23 14:55:32 +0900

    Add initialization for rbenv into the build script

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,7 +1,18 @@
 module JobsHelper
   def combined_scripts(job_setting)
-    %w[before_script script after_script].map {|type|
-      "# #{type}\n" + [job_setting[type]].join("\n") + "\n"
-    }.join("\n")
+    [ rbenv_setup_script,
+      %w[before_script script after_script].reject {|type|
+        job_setting[type].nil? || job_setting[type].strip.empty?
+      }.map {|type|
+        "# #{type}\n" + [job_setting[type]].join("\n") + "\n"
+      },
+    ].join("\n")
+  end
+
+  def rbenv_setup_script
+    <<-'EOS'
+PATH=~/.rbenv/bin:$PATH
+eval "$(rbenv init -)"
+    EOS
   end
 end
